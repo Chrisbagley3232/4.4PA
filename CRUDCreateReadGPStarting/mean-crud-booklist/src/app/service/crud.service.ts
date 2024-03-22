5367ed9df9475f9c0f0c56545f3d860a34f76ba9
Chrisbagley3232 <Chris32321089@gmail.com>
2024-03-22 17:42:36 -0400

    first commit

--- CRUDCreateReadGPStarting/mean-crud-booklist/src/app/service/crud.service.ts
+++ CRUDCreateReadGPStarting/mean-crud-booklist/src/app/service/crud.service.ts
@@ -22,6 +22,15 @@ export class CrudService {
   GetBooks() {
     return this.httpClient.get(`${this.REST_API}`);
   }
+
+  // Add
+  AddBook(data: Book): Observable<any> {
+    let API_URL = `${this.REST_API}/add-book`;
+    return this.httpClient.post(API_URL, data)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
  
   // Error 
   handleError(error: HttpErrorResponse) {
